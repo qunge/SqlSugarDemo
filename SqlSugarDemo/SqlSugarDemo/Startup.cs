d5c5fccf5b52aae152d2020000c85420089d374a
qunge <qunge1@outlook.com>
2018-01-31 23:24:10 +0800

    第一次

---
+++ SqlSugarDemo/SqlSugarDemo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SqlSugarDemo.Startup))]
+namespace SqlSugarDemo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
