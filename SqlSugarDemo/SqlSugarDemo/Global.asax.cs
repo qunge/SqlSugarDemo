bab0df8dcb6e900ddcd4ae06da752b5b1f8708a8
qunge <qunge1@outlook.com>
2018-02-01 15:19:12 +0800

    新增登录界面

--- SqlSugarDemo/SqlSugarDemo/Global.asax.cs
+++ SqlSugarDemo/SqlSugarDemo/Global.asax.cs
@@ -13,9 +13,9 @@ namespace SqlSugarDemo
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
-            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            //FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
-            BundleConfig.RegisterBundles(BundleTable.Bundles);
+            //BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
     }
 }
